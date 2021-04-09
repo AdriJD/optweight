fixed bug in wlm2alm single precision not using ncomp > 0 wlms, added lmul_pix function and some changes to estimate_cov_pix

optweight/map_utils.py
@@ -7,7 +7,7 @@ from scipy.interpolate import NearestNDInterpolator, RectBivariateSpline
 from pixell import enmap, sharp, utils, wcsutils
 import healpy as hp
 
-from optweight import wavtrans
+from optweight import wavtrans, sht, mat_utils
 
 def view_2d(imap, minfo):
     '''
@@ -393,7 +393,7 @@ def rand_map_pix(cov_pix):
     npol, npix = cov_pix.shape[-2:]
 
     # Draw (npol, npix) unit variates for map.
-    uv = np.random.randn(npol * npix).reshape(npol, npix)
+    uv = np.random.randn(npol, npix).astype(cov_pix.dtype)
 
     if cov_pix.ndim == 2:
         cov_pix_sqrt = np.sqrt(cov_pix)
@@ -403,9 +403,7 @@ def rand_map_pix(cov_pix):
         # We use SVD (=eigenvalue decomposition for positive-semi-definite matrices)
         # because some pixels are zero and (i)cov is positive *semi*-definite
         # instead of *definite*, so Cholesky won't work.    
-        cov_pix_t = np.transpose(cov_pix, (2, 0, 1))
-        cov_pix_sqrt_t = utils.eigpow(cov_pix_t, 0.5)
-        cov_pix_sqrt = np.ascontiguousarray(np.transpose(cov_pix_sqrt_t, (1, 2, 0)))    
+        cov_pix_sqrt = mat_utils.matpow(cov_pix, 0.5)
         uv = np.einsum('ijk, jk -> ik', cov_pix_sqrt, uv, optimize=True)
 
     return uv
@@ -768,3 +766,46 @@ def inpaint_nearest(imap, mask, minfo):
         omap[pidx,~mask[midx]] = ndi(pix_masked[midx][0], pix_masked[midx][1])
 
     return omap.reshape(shape_in)
+
+def lmul_pix(imap, lmat, minfo, spin, inplace=False):
+    '''
+    Convert map to spherical harmonic domain, apply matrix and convert back.
+
+    Arguments
+    ---------
+    imap : (npol, npix)
+        Input map.
+    lmat : (npol, npol, nell) or (npol, nell) array
+        Matrix in multipole domain, if diagonal only the diaganal suffices.
+    minfo : sharp.map_info object
+        Metainfo input map.
+    spin : int, array-like
+        Spin values for transform, should be compatible with npol.    
+    inplace : bool, optional
+        If set, use input map array for output.
+
+    Returns
+    -------
+    omap : (npol, npix)
+        Output map.
+    '''
+
+    if imap.ndim == 1:
+        imap = imap[np.newaxis,:]
+    npol = imap.shape[0]
+
+    lmax = nell.shape[-1] - 1
+    ainfo = sharp.alm_info(lmax)
+    alm = np.zeros((npol, ainfo.nelem))
+
+    sht.map2alm(imap, alm, minfo, ainfo, spin, adjoint=False)
+    alm_c_utils.lmul(alm, lmat, ainfo, inplace=True)
+
+    if inplace:
+        omap = imap
+    else:
+        omap = np.zeros_like(imap)
+
+    sht.alm2map(alm, omap, ainfo, minfo, spin, adjoint=False)
+    
+    return omap

optweight/noise_utils.py
@@ -27,15 +27,94 @@ def noisealm2wavmat():
 
 # estimate var
 
-def noisemap2var():
+def estimate_cov_pix(imap, minfo, mask=None, diag=False, cov_pix_template=None):
+    '''
+    Estimate noise covariance (uncorelated between pixels, possible correlated 
+    between components).
+
+    Arguments
+    ---------
+    imap : (npol, npix)
+        Input noise map.
+    minfo : sharp.map_info object
+        Metainfo input mask.
+    mask : (npol, npix) or (npix) bool array, optional
+        Mask, True for unmasked (good) data.
+    diag : bool, optional
+        If set, only estimate elements diagonal in pol.
+    cov_pix_template : (npol, npol, npix) or (npol, npix), optional
+        Template covariance matrix used to flatten noise before smoothing
+        to avoid biases around sharp transitions.
+
+    Returns
+    -------
+    cov_pix : (npol, npol, npix) or (npol, npix)
+        Estimated covariance matrix, only diagonal if diag is set.
+    '''
     
-    # Determine mask
+    if imap.ndim == 1:
+        imap = imap[np.newaxis,:]
+
+    if mask is None:
+        mask = imap == 0
+    elif mask.ndim == 1:
+        mask = mask[np.newaxis,:]
+
+    if diag:
+        cov_pix = imap ** 2
+    else:
+        cov_pix_est = np.einsum('il, kl -> ikl', imap, imap,
+                                optimize=True)
+
+    if cov_pix_template:
+
+        #if diag:
+            
+
+        # if 2d and cov_pix is 3d...
+        # create cross of icov_pix?? NO just do array multiplation.
+
+        # if 3d and cov_pix is 2d: select diagonal template
+
+        # array mul
+
+        # if both 3d
+
+        #icov_pix_template = mat_utils.matpow(cov_pix_template, -1)
+        #cov_pix_est = np.einsum('ijk, jlk -> ilk', icov_pix_template,
+        #                        cov_pix_est, optimze=True)
+        
+        pass
+
+    # Presmooth with small beam.
+    
+    # DETERMINE SMALL BEAM...
+    b_ell = hp.gauss_beam(np.radians(1), lmax=lmax)
+
+    # DETERMINE BEAM..
+
+    # ONLY KEEP UPPER_TRIANGLE??? YES saves a bunch of SHTs.
+    # LOOP OVER UPPER TRIANGLE.
+    map_utils.lmul_pix(cov_pix_est.reshape(), b_ell, minfo, np.zeros(), inplace=False)
+
+    #for pidx in range(3):
+    #    for pjdx in range(3):
+        
+    #        # Only use spin 0 transforms..
+    #        alm = alm_noise[0].copy()
+    #        sht.map2alm(cov_pix_est[pidx,pjdx], alm, minfo, ainfo, 0, adjoint=False)
+    #        alm_c_utils.lmul(alm, b_ell, ainfo, inplace=True)
+    #        sht.alm2map(alm, cov_pix_est_presm[pidx,pjdx], ainfo, minfo, 0, adjoint=False)
 
     # inpaint
 
     # smooth
 
-    # find closest psd matrix
+    # end loop over upper triangle
+
+    # fill lower triangle.
+
+    # find closest psd matrix <<-- probbaly skip this for now.
     
     pass
 

optweight/wlm_utils.py
@@ -48,7 +48,7 @@ def get_sd_kernels(lamb, lmax, j0=None, lmin=None, jmax=None, lmax_j=None,
     -----
     Wavelet numbers J run from 0 to jmax if j0 or lmin is not specified.
     The lowest wavelet number corresponds to Phi (i.e. the scaling kernel that
-    that starts as a constant of 1 at ell = 0. This differs from the s2let code.
+    starts as a constant of 1 at ell = 0. This differs from the s2let code.
     Optionally, the last wavelet scale can be "omega", i.e. a kernel that is 1
     for all ell above lambda.
     '''
@@ -161,7 +161,15 @@ def lmax_to_j_scale(lmax, lamb):
     -------
     j_scale : int
         Wavelet number.
+    
+    Raises
+    ------
+    ValueError
+        If lambda <= 1.
     '''
+
+    if lamb <= 1:
+        raise ValueError(f'lambda needs to be > 1, got {lamb}')
 
     return int(np.ceil(np.log10(lmax) / np.log10(lamb)))
 

tests/test_alm_c_utils.py
@@ -291,34 +291,75 @@ class TestAlmCUtils(unittest.TestCase):
     def test_wlm2alm_dp(self):
         
         alm = np.ones((3, 10), dtype=np.complex128)
+        alm[1] *= 2
+        alm[2] *= 3
         wlm = np.ones((3, 6), dtype=np.complex128) * 1j
+        wlm[1] *= 10
+        wlm[2] *= 100
         wlm *= np.arange(6)
         w_ell = np.arange(4, dtype=np.float64)
         w_ell[-1] = 0
         alm_exp = np.zeros_like(alm)
-        alm_exp[:] = np.asarray(
+        alm_exp[0] = np.asarray(
             [1, 1 + 1j, 1 + 4j, 1, 1 + 3j, 1 + 8j, 1, 1 + 10j, 1, 1],
+            dtype=np.complex128)[np.newaxis,:]
+
+        alm_exp[1] = np.asarray(
+            [2, 2 + 10j, 2 + 40j, 2, 2 + 30j, 2 + 80j, 2, 2 + 100j, 2, 2],
+            dtype=np.complex128)[np.newaxis,:]
+
+        alm_exp[2] = np.asarray(
+            [3, 3 + 100j, 3 + 400j, 3, 3 + 300j, 3 + 800j, 3, 3 + 1000j, 3, 3],
             dtype=np.complex128)[np.newaxis,:]
 
         alm_c_utils.wlm2alm(w_ell, wlm, alm, 2, 3)
         
-        np.testing.assert_array_almost_equal(alm, alm_exp)
+        np.testing.assert_allclose(alm, alm_exp)
 
     def test_wlm2alm_sp(self):
         
+        # alm = np.ones((3, 10), dtype=np.complex64)
+        # wlm = np.ones((3, 6), dtype=np.complex64) * 1j
+        # wlm *= np.arange(6)
+        # w_ell = np.arange(4, dtype=np.float32)
+        # w_ell[-1] = 0
+        # alm_exp = np.zeros_like(alm)
+        # alm_exp[:] = np.asarray(
+        #     [1, 1 + 1j, 1 + 4j, 1, 1 + 3j, 1 + 8j, 1, 1 + 10j, 1, 1],
+        #     dtype=np.complex64)[np.newaxis,:]
+
+        # alm_c_utils.wlm2alm(w_ell, wlm, alm, 2, 3)
+        
+        # np.testing.assert_array_almost_equal(alm, alm_exp)
+
         alm = np.ones((3, 10), dtype=np.complex64)
+        alm[1] *= 2
+        alm[2] *= 3
         wlm = np.ones((3, 6), dtype=np.complex64) * 1j
+        wlm[1] *= 10
+        wlm[2] *= 100
         wlm *= np.arange(6)
         w_ell = np.arange(4, dtype=np.float32)
         w_ell[-1] = 0
         alm_exp = np.zeros_like(alm)
-        alm_exp[:] = np.asarray(
+        alm_exp[0] = np.asarray(
             [1, 1 + 1j, 1 + 4j, 1, 1 + 3j, 1 + 8j, 1, 1 + 10j, 1, 1],
+            dtype=np.complex64)[np.newaxis,:]
+
+        alm_exp[1] = np.asarray(
+            [2, 2 + 10j, 2 + 40j, 2, 2 + 30j, 2 + 80j, 2, 2 + 100j, 2, 2],
+            dtype=np.complex64)[np.newaxis,:]
+
+        alm_exp[2] = np.asarray(
+            [3, 3 + 100j, 3 + 400j, 3, 3 + 300j, 3 + 800j, 3, 3 + 1000j, 3, 3],
             dtype=np.complex64)[np.newaxis,:]
 
         alm_c_utils.wlm2alm(w_ell, wlm, alm, 2, 3)
         
-        np.testing.assert_array_almost_equal(alm, alm_exp)
+        print(alm)
+        print(alm_exp)
+
+        np.testing.assert_allclose(alm, alm_exp)
 
     def test_wlm2alm_err(self):
         