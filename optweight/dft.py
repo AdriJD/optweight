9d31c9bc7db0d01dba4589c7ad9faf796ce7a8ca
AdriJD <adri.j.duivenvoorden@gmail.com>
2022-04-01 09:56:57 -0400

    added dft.py that contains basic rFFT routines

---
+++ optweight/dft.py
@@ -0,0 +1,199 @@
+'''
+Routines for real-to-complex and complex-to-real FFTs. Adapted from pixell and mnms.
+'''
+import numpy as np
+
+from pixell import fft, enmap, wcsutils
+
+def rfft(emap, fmap, normalize=True, adjoint=False):
+    '''
+    Real-to-complex FFT.
+
+    Parameters
+    ----------
+    emap : (..., ny, nx) ndmap
+        Map to transform.
+    fmap : (..., ny, nx//2+1) complex ndmap
+        Output buffer.
+    normalize : bool, optional
+        The FFT normalization, by default True. If True, normalize
+        using pixel number. If in ['phy', 'phys', 'physical'],
+        normalize by sky area.
+    adjoint : bool, optional
+        Compute adjoint of the complex-to-real FFT.
+    '''
+
+    fmap = fft.rfft(emap, fmap, axes=[-2, -1])
+    norm = 1
+
+    if normalize:
+        norm /= np.prod(emap.shape[-2:]) ** 0.5
+    if normalize in ["phy","phys","physical"]:
+        if adjoint:
+            norm /= emap.pixsize() ** 0.5
+        else:
+            norm *= emap.pixsize() ** 0.5
+    if norm != 1:
+        fmap *= norm
+
+def irfft(fmap, omap=None, normalize=True, adjoint=False, destroy_input=False):
+    '''
+    Complex-to-real FFT.
+
+    Parameters
+    ----------
+    fmap : (..., ny, nx//2+1) complex ndmap
+        Input Fourier coefficients.
+    omap : (..., ny, nx) ndmap
+        Output buffer.
+    normalize : bool, optional
+        The FFT normalization, by default True. If True, normalize
+        using pixel number. If in ['phy', 'phys', 'physical'],
+        normalize by sky area.
+    adjoint : bool, optional
+        Compute the adjoint of the real-to-complex FFT.
+    destroy_input : bool, optional
+        If set, input `fmap` array might be overwritten.
+    '''
+
+    if not destroy_input:
+        fmap = fmap.copy()
+    omap = fft.irfft(fmap, omap, axes=[-2, -1], normalize=False)
+    norm = 1
+
+    if normalize:
+        norm /= np.prod(omap.shape[-2:]) ** 0.5
+    if normalize in ["phy","phys","physical"]:
+        if adjoint:
+            norm *= fmap.pixsize() ** 0.5
+        else:
+            norm /= fmap.pixsize() ** 0.5
+    if norm != 1:
+        omap *= norm
+
+def laxes_real(shape, wcs):
+    '''
+    Compute ell_x and ell_y axes corresponding to a given enmap geometry.
+
+    Arguments
+    ---------
+    shape : tuple
+        Shape of geomtry.
+    wcs : astropy.wcs.WCS object
+        WCS object describing geometry.
+
+    Returns
+    -------
+    ly : (nly) array
+        Wavenumbers in y direction.
+    lx : (nlx) array
+        Wavenumbers in x direction.
+    '''
+
+    step = enmap.extent(shape, wcs, signed=True, method='auto') / shape[-2:]
+    ly = np.fft.fftfreq(shape[-2], step[0]) * 2 * np.pi
+    lx = np.fft.rfftfreq(shape[-1], step[1]) * 2 * np.pi
+
+    return ly, lx
+
+def lmap_real(shape, wcs, dtype=np.float64):
+    '''
+    Return maps of all the wavenumbers corresponding to a given enmap geometry.
+
+    Arguments
+    ---------
+    shape : tuple
+        Shape of geomtry.
+    wcs : astropy.wcs.WCS object
+        WCS object describing geometry.
+    dtype : type, optional
+        Type of ouput array.
+
+    Returns
+    -------
+    lmap : (2, nly, nlx) array
+       Maps of ell_y and ell_x wavenumbers.
+    '''
+
+    ly, lx = laxes_real(shape, wcs)
+    lmap = np.empty((2, ly.size, lx.size), dtype=dtype)
+    lmap[0] = ly[:,np.newaxis]
+    lmap[1] = lx[np.newaxis,:]
+
+    return lmap
+
+def modlmap_real(shape, wcs, dtype=np.float64):
+    '''
+    Return a map of all the abs wavenumbers in the fourier transform
+    of a map with the given shape and wcs.
+
+    Arguments
+    ---------
+    shape : tuple
+        Shape of geomtry.
+    wcs : astropy.wcs.WCS object
+        WCS object describing geometry.
+    dtype : type, optional
+        Type of ouput array.
+
+    Returns
+    -------
+    lmod : (nly, nlx) array
+       Map of absolute wavenumbers.
+    '''
+
+    slmap = lmap_real(shape, wcs, dtype=dtype)
+    lmod = np.sum(slmap ** 2, axis=0) ** 0.5
+
+    return lmod
+
+def lwcs_real(shape, wcs):
+    '''
+    Build world coordinate system for Fourier space (with reality symmetry) given
+    enmap geometry.
+
+    Arguments
+    ---------
+    shape : tuple
+        Shape of geomtry.
+    wcs : astropy.wcs.WCS object
+        WCS object describing geometry.
+
+    Returns
+    -------
+    lwcs : astropy.wcs.WCS object
+        WCS object describing Fourier space geometry.
+    '''
+
+    lres = 2 * np.pi / enmap.extent(shape, wcs, signed=True)
+    ny = shape[-2]
+    nx = shape[-1] // 2 + 1
+    lres[-1] *= (nx / shape[-1])
+
+    return wcsutils.explicit(crpix=[0,ny//2+1], crval=[0,0], cdelt=lres[::-1])
+
+def lbin(fmap, lmod, bsize=None):
+    '''
+    Bin Fourier-space map into radial bins.
+    
+    Arguments
+    ---------
+    fmap : (..., nly, nlx) array.
+        Input 2D Fourier map.
+    lmod : (nly, nlx) array
+        Map of absolute wavenumbers.
+    bsize : float, optional
+        Radial bin size. Defaults to resolution in ell of input map.
+
+    Returns
+    -------
+    fbin : (..., nbin) array
+        Radially binned input.
+    bins : (nbin) array
+        Bins.
+    '''
+
+    if bsize is None:
+        bsize = min(abs(lmod[0,1]), abs(lmod[1,0]))
+
+    return enmap._bin_helper(fmap, lmod, bsize)
---
+++ tests/test_dft.py
@@ -0,0 +1,154 @@
+import unittest
+import numpy as np
+
+from pixell import enmap, fft
+
+from optweight import dft
+
+class TestSHT(unittest.TestCase):
+
+    def test_rfft(self):
+        
+        # Test roundtrip.
+        ny = 10
+        nx = 11
+        res = [np.pi / (ny - 1), 2 * np.pi / nx]
+        dec = np.radians([-60, 40])
+        shape, wcs = enmap.band_geometry(dec, res=res, shape=(ny, nx), dims=(2,))
+        imap = enmap.ones(shape, wcs)
+
+        fmap = np.zeros(shape[:-1] + (shape[-1] // 2 + 1,), np.complex128)
+        
+        dft.rfft(imap, fmap)
+        fmap_copy = fmap.copy()
+
+        omap = np.zeros_like(imap)
+        dft.irfft(fmap, omap)
+        
+        np.testing.assert_allclose(imap, omap)
+        # Test if c2r transform does not overwrite input.
+        np.testing.assert_allclose(fmap, fmap_copy)
+
+    def test_rfft_sp(self):
+        
+        # Test roundtrip with single precision.
+        ny = 10
+        nx = 11
+        res = [np.pi / (ny - 1), 2 * np.pi / nx]
+        dec = np.radians([-60, 40])
+        shape, wcs = enmap.band_geometry(dec, res=res, shape=(ny, nx), dims=(2,))
+        imap = enmap.ones(shape, wcs, dtype=np.float32)
+
+        fmap = np.zeros(shape[:-1] + (shape[-1] // 2 + 1,), np.complex64)
+        
+        dft.rfft(imap, fmap)
+        fmap_copy = fmap.copy()
+
+        omap = np.zeros_like(imap)
+        dft.irfft(fmap, omap)
+        
+        np.testing.assert_allclose(imap, omap)
+        np.testing.assert_allclose(fmap, fmap_copy)
+
+        self.assertEqual(imap.dtype, np.float32)
+        self.assertEqual(omap.dtype, np.float32)
+        self.assertEqual(fmap.dtype, np.complex64)
+        
+    def test_laxes_real(self):
+        
+        ny = 6
+        nx = 11
+        shape, wcs = enmap.geometry([0,0], res=0.1, deg=True, shape=(ny, nx))
+
+        ly, lx = dft.laxes_real(shape, wcs)
+
+        ly_exp = np.fft.fftfreq(ny, np.radians(0.1)) * 2 * np.pi
+        lx_exp = np.fft.rfftfreq(nx, -np.radians(0.1)) * 2 * np.pi
+        
+        np.testing.assert_allclose(ly, ly_exp, atol=1e-3, rtol=1e-5)
+        np.testing.assert_allclose(lx, lx_exp, atol=1e-3, rtol=1e-5)
+
+    def test_lmap_real(self):
+        
+        ny = 6
+        nx = 11
+        shape, wcs = enmap.geometry([0,0], res=0.1, deg=True, shape=(ny, nx))
+
+        ly_exp = np.fft.fftfreq(ny, np.radians(0.1)) * 2 * np.pi
+        lx_exp = np.fft.rfftfreq(nx, -np.radians(0.1)) * 2 * np.pi
+        lmap_exp = np.zeros((2, ny, nx // 2 + 1))
+        lmap_exp[0] = ly_exp[:,np.newaxis]
+        lmap_exp[1] = lx_exp[np.newaxis,:]
+            
+        lmap = dft.lmap_real(shape, wcs)
+
+        self.assertEqual(lmap.shape, (2, ny, nx // 2 + 1))
+        np.testing.assert_allclose(lmap, lmap_exp, atol=1e-3, rtol=1e-5)
+        self.assertEqual(lmap.dtype, np.float64)
+    
+        lmap = dft.lmap_real(shape, wcs, dtype=np.float32)
+        self.assertEqual(lmap.dtype, np.float32)
+        np.testing.assert_allclose(
+            lmap, lmap_exp.astype(np.float32), atol=1e-3, rtol=1e-5)
+
+    def test_modlmap_real(self):
+        
+        ny = 6
+        nx = 11
+        shape, wcs = enmap.geometry([0,0], res=0.1, deg=True, shape=(ny, nx))
+
+        ly_exp = np.fft.fftfreq(ny, np.radians(0.1)) * 2 * np.pi
+        lx_exp = np.fft.rfftfreq(nx, -np.radians(0.1)) * 2 * np.pi
+        lmap_exp = np.zeros((2, ny, nx // 2 + 1))
+        lmap_exp[0] = ly_exp[:,np.newaxis]
+        lmap_exp[1] = lx_exp[np.newaxis,:]
+        lmod_exp = np.zeros((ny, nx // 2 + 1))
+        lmod_exp = np.sqrt(lmap_exp[0] ** 2 + lmap_exp[1] ** 2)
+
+        lmod = dft.modlmap_real(shape, wcs)
+
+        np.testing.assert_allclose(lmod, lmod_exp, atol=1e-3, rtol=1e-5)
+
+        lmod = dft.modlmap_real(shape, wcs, dtype=np.float32)
+        self.assertEqual(lmod.dtype, np.float32)
+        np.testing.assert_allclose(
+            lmod, lmod_exp.astype(np.float32), atol=1e-3, rtol=1e-5)
+        
+    def test_lwcs_real(self):
+
+        ny = 6
+        nx = 11
+        shape, wcs = enmap.geometry([0,0], res=0.1, deg=True, shape=(ny, nx))
+        
+        lwcs = dft.lwcs_real(shape, wcs)
+
+        # Note that wcs order is x, y. Center pix y is 6 // 2 + 1 = 4.
+        np.testing.assert_allclose(lwcs.wcs.crpix, np.asarray([0., 4.]))
+        np.testing.assert_allclose(lwcs.wcs.crval, np.asarray([0., 0.]))        
+        cdelt_exp = 2 * np.pi / np.radians([1.1, 0.6])
+        cdelt_exp[0] *= -(6 / 11) # 6 is 11 // 2 + 1.
+        np.testing.assert_allclose(lwcs.wcs.cdelt, cdelt_exp, atol=1e-3, rtol=1e-5)
+
+    def test_lbin(self):
+        
+        ny = 6
+        nx = 11
+        shape, wcs = enmap.geometry([0,0], res=0.1, deg=True, shape=(ny, nx))
+
+        ly_exp = np.fft.fftfreq(ny, np.radians(0.1)) * 2 * np.pi
+        lx_exp = np.fft.rfftfreq(nx, -np.radians(0.1)) * 2 * np.pi
+        lmap_exp = np.zeros((2, ny, nx // 2 + 1))
+        lmap_exp[0] = ly_exp[:,np.newaxis]
+        lmap_exp[1] = lx_exp[np.newaxis,:]
+        lmod_exp = np.zeros((ny, nx // 2 + 1))
+        lmod_exp = np.sqrt(lmap_exp[0] ** 2 + lmap_exp[1] ** 2)
+
+        fmap = np.ones((2, ny, nx // 2 + 1))
+
+        fbin, bins = dft.lbin(fmap, lmod_exp, bsize=500)
+
+        fbin_exp = np.ones((2, 4))
+        bins_exp = np.asarray([0., 591.45389248, 1105.18002676, 1514.60855085])
+
+        np.testing.assert_allclose(bins, bins_exp)
+        np.testing.assert_allclose(fbin, fbin_exp)
