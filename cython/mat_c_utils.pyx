3f45ee362a51ddfbec8c19d44a98b0927fce49ff
AdriJD <adri.j.duivenvoorden@gmail.com>
2022-11-21 00:08:45 -0500

    added files for C mat_utils

---
+++ cython/mat_c_utils.pyx
@@ -0,0 +1,17 @@
+
+
+# Wrap dsyev, cheev
+
+# Wrap dgemm or whatever it's called
+
+def eigpow():
+    '''
+
+    '''
+
+    # Copy of Sigurds enlib code.
+
+    # Including openmp part.
+
+    pass
+
