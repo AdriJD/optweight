fixed f2wav and wav2f, wav objects should contain flattened maps. Changed sign conventions of lwcs

optweight/dft.py
@@ -220,7 +220,8 @@ def lwcs_real(shape, wcs):
         WCS object describing Fourier space geometry.
     '''
 
-    lres = 2 * np.pi / enmap.extent(shape, wcs, signed=True)
+    lres = 2 * np.pi / (np.radians(wcs.wcs.cdelt[::-1]) * shape[-2:])
+    lres[-1] = abs(lres[-1])
     ny = shape[-2]
     
     return wcsutils.explicit(crpix=[0,ny//2+1], crval=[0,0], cdelt=lres[::-1])
@@ -372,7 +373,7 @@ def cl2flat(c_ell, ells, modlmap):
     cs = interp1d(ells, c_ell, kind='linear', assume_sorted=True,
                   bounds_error=True)
 
-    return cs(modlmap)
+    return cs(modlmap).astype(c_ell.dtype, copy=False)
 
 def calc_ps1d(fmap, wcs, modlmap, fmap2=None, bsize=None):
     '''

optweight/noise_utils.py
@@ -4,7 +4,7 @@ from scipy.interpolate import CubicSpline
 from pixell import enmap, sharp
 import healpy as hp
 
-from optweight import wavtrans, map_utils, mat_utils, type_utils, wlm_utils, sht
+from optweight import wavtrans, map_utils, mat_utils, type_utils, wlm_utils, sht, dft
 
 def estimate_cov_wav(alm, ainfo, w_ell, spin, diag=False, wav_template=None,
                      fwhm_fact=2):
@@ -115,7 +115,7 @@ def estimate_cov_fwav(fmap, fkernels, modlmap, wav_template, diag=False,
     noise_wav = wavtrans.f2wav(fmap, wav_template, fkernels)
 
     # Insert monopole back in.
-    fmap[...,0,0] = fmap
+    fmap[...,0,0] = fmap_mono
 
     cov_wav = wavtrans.Wav(2, dtype=type_utils.to_real(fmap.dtype))
 
@@ -130,13 +130,14 @@ def estimate_cov_fwav(fmap, fkernels, modlmap, wav_template, diag=False,
         index = (jidx, jidx)
         minfo = noise_wav.minfos[jidx]
         
-        _lmax = map_utils.minfo2lmax(minfo)
+        _lmax = int(np.max(modlmap[fkernels[jidx] > 1e-6]))
+
         fwhm = _fwhm_fact(_lmax) * np.pi / _lmax
         cov_pix = estimate_cov_pix(noise_wav.maps[jidx], minfo, diag=diag,
                                    fwhm=fwhm, flatsky=True, modlmap=modlmap)
         # Correct for kernel shape.
-        cov_wav /= np.mean(np.abs(fkernels[jidx]) ** 2)
-
+        cov_pix /= np.mean(np.abs(fkernels[jidx]) ** 2)
+        
         cov_wav.add(index, cov_pix, minfo)
 
     return cov_wav
@@ -157,7 +158,7 @@ def estimate_cov_pix(imap, minfo, diag=False, fwhm=None, lmax=None,
         If set, only estimate elements diagonal in pol.
     fwhm : float, optional
         Specify the FWHM (in radians) of the Gaussian used for smoothing,
-        default is 2 * pi / lmax. If features is given, this parameter is ignored.
+        default is 2 * pi / lmax.
     lmax : int, optional
         If set, assume that input noise map has this harmonic band limit.
     kernel_ell : (nell) array, optional
@@ -195,9 +196,10 @@ def estimate_cov_pix(imap, minfo, diag=False, fwhm=None, lmax=None,
     else:
         cov_pix = np.einsum('abc, dec -> abdec', imap, imap, optimize=True)
 
+    print(np.degrees(fwhm))
     b_ell = _band_limit_gauss_beam(map_utils.minfo2lmax(minfo), fwhm=fwhm)
     if flatsky:
-        fb = dft.cl2flat(b_ell, np.arange(lmax + 1), modlmap)
+        fb = dft.cl2flat(b_ell, np.arange(b_ell.shape[-1]), modlmap)
 
     if not diag:
         cov_pix = cov_pix.reshape(ncomp * npol, ncomp * npol, npix)

tests/test_dft.py
@@ -171,7 +171,7 @@ class TestSHT(unittest.TestCase):
         np.testing.assert_allclose(lwcs.wcs.crpix, np.asarray([0., 4.]))
         np.testing.assert_allclose(lwcs.wcs.crval, np.asarray([0., 0.]))        
         cdelt_exp = 2 * np.pi / np.radians([1.1, 0.6])
-        cdelt_exp[0] *= -1
+
         np.testing.assert_allclose(lwcs.wcs.cdelt, cdelt_exp, atol=1e-3, rtol=1e-5)
 
     def test_lbin(self):

tests/test_wavtrans.py
@@ -904,9 +904,12 @@ class TestWavTransIO(unittest.TestCase):
         fmap = np.zeros((1,) + modlmap.shape, dtype=np.complex128)
         dft.rfft(imap, fmap)
 
+        # Reshape imap, we need flattened ny, nx for wav.
+        imap_tmp = imap.reshape(imap.shape[0], imap.shape[1] * imap.shape[2])
+
         wav = wavtrans.Wav(1)
-        wav.add(0, imap * 0, minfo)
-        wav.add(1, imap * 0, minfo)
+        wav.add(0, imap_tmp * 0, minfo)
+        wav.add(1, imap_tmp * 0, minfo)
 
         wav = wavtrans.f2wav(fmap, wav, fkernels)
         
@@ -935,9 +938,12 @@ class TestWavTransIO(unittest.TestCase):
         fmap = np.zeros((1,) + modlmap.shape, dtype=np.complex128)
         dft.rfft(imap, fmap)
 
+        # Reshape imap, we need flattened ny, nx for wav.
+        imap_tmp = imap.reshape(imap.shape[0], imap.shape[1] * imap.shape[2])
+
         wav = wavtrans.Wav(1)
-        wav.add(0, imap * 0, minfo)
-        wav.add(1, imap * 0, minfo)
+        wav.add(0, imap_tmp * 0, minfo)
+        wav.add(1, imap_tmp * 0, minfo)
 
         wav = wavtrans.f2wav(fmap, wav, fkernels)
 