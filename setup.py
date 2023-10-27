d79702cc298af48c76e90b1cf4e31b20a39d2f0e
AdriJD <adri.j.duivenvoorden@gmail.com>
2023-10-27 18:59:47 -0400

    increased version number

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ ext_modules = [Extension('optweight.alm_c_utils',
 
 setup(name='optweight',
       packages=['optweight'],
-      version='0.0.1',
+      version='0.0.2',
       cmdclass=cmdclass,
       ext_modules=cythonize(ext_modules, annotate=True,
                             compiler_directives=compiler_directives))
